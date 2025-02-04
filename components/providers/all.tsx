1da9594e04eb8d28d0e3c2efc0702821bed1289f
Ajay Singh <admin@ajaysingh.com.np>
2025-02-04 20:44:58 +0545

    dark mode and themeing initialized

--- components/providers/all.tsx
+++ components/providers/all.tsx
@@ -1,8 +1,17 @@
-// src/components/providers.tsx
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from './themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
+  return <SessionProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  </SessionProvider>
 }
