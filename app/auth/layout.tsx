0613261e3a18ffe597002d1458a5f9d689bda4d1
Ajay Singh <admin@ajaysingh.com.np>
2025-02-07 01:53:50 +0545

    copyright added to auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,13 +1,15 @@
+import CopyRight from '@/components/blocks/copyright';
 import { ModeToggle } from '@/components/ui/theme-toggle';
 import React from 'react'
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 relative">
       <div className="absolute top-0 right-0 p-4">
         <ModeToggle />
       </div>
       {children}
+      <CopyRight />
     </div>
   )
 }
