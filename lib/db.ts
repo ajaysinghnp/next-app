d0881a550a27b5492ac4ce79fdb21da34eeeeeeb
Ajay Singh <admin@ajaysingh.com.np>
2025-02-15 21:25:08 +0545

    eslint setup complete

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
+import { env } from "@/env/server";
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const db = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
