50d856fad73884f7643ae4f976f07c98ee6c5b40
Ajay Singh <ajay@nrb.org.np>
2025-02-18 14:27:48 +0545

    authentication form card refactored

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/authentication/login-form";
 
 export default function Login() {
   return (
-    <main className="w-full max-w-sm drop-shadow-md">
+    <main className="w-full max-w-sm drop-shadow-md shadow-slate-800 shadow-md">
       <LoginForm />
     </main>
   );
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "@/components/authentication/register-form";
 
 export default function Register() {
   return (
-    <main className="w-full max-w-sm drop-shadow-md">
+    <main className="w-full max-w-sm drop-shadow-md shadow-slate-800 shadow-md">
       <RegisterForm />
     </main>
   );
