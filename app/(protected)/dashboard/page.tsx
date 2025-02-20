4cca6d923e0bb9a7a30dd09af2da8548eca10c0a
Ajay Singh <ajay@nrb.org.np>
2025-02-20 11:31:00 +0545

    pre formatting added for the json in the dashboard temporarily

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -4,7 +4,7 @@ export default async function Dashboard() {
   const session = await auth();
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <p>{JSON.stringify(session)}</p>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </main>
   );
 }
